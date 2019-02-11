a6799b9c3591d6ed108e6fe2b11ab0cdcf1f5140
Martijn van Ginkel <>
2019-02-11 18:59:50 +0100

    Added UI buttons to interactable flower

---
+++ Assets/Scripts/Objects/InteractableObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class InteractableObject : MonoBehaviour
+{
+
+    public delegate void PlayerOnObject(bool onObject);
+    public static event PlayerOnObject OnPlayerOnObject;
+
+    [SerializeField] private GameObject m_ButtonPanel;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        m_ButtonPanel.SetActive(false);
+    }
+
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entered");
+        OnPlayerOnObject?.Invoke(true);
+        m_ButtonPanel.SetActive(true);
+    }
+
+    protected void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Entered");
+        OnPlayerOnObject?.Invoke(false);
+        m_ButtonPanel.SetActive(false);
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,29 +9,46 @@ public class PlayerController : MonoBehaviour
     public static event PlayerAction OnPlayerAction;
 
     private Rigidbody2D m_RigidBody;
+    private Animator m_Animator;
+    private SpriteRenderer m_SpriteRenderer;
 
     [SerializeField] private float m_MoveSpeed;     
     [SerializeField] private bool m_AllowInput = true;
+    private bool m_FacingRight = true;
+
+    [SerializeField] private bool m_ActionAllowed;
 
 
     void Start()
     {
         m_RigidBody = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
     {
         CameraController.OnCameraMove += DisablePlayer;
+        InteractableObject.OnPlayerOnObject += AllowPlayer;
     }
     
     private void OnDisable()
     {
         CameraController.OnCameraMove -= DisablePlayer;
+        InteractableObject.OnPlayerOnObject -= AllowPlayer;
     }
 
     private void Update()
     {
-        GetInput();
+        GetInput(); // Always check for input of the player
+
+        m_Animator.SetFloat("MoveSpeed", Mathf.Abs(m_RigidBody.velocity.x));
+    }
+
+    private void AllowPlayer(bool onInteractableObject) // TO DO : different name
+    {
+        m_ActionAllowed = onInteractableObject;
+        Debug.Log("Allow");
     }
 
 
@@ -49,16 +66,28 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 m_RigidBody.velocity = new Vector2(m_MoveSpeed, m_RigidBody.velocity.y);
+                if (!m_FacingRight)
+                {
+                    m_FacingRight = true;
+                    m_SpriteRenderer.flipX = false;
+                }
             }
             else if (Input.GetKey(KeyCode.LeftArrow))
             {
                 m_RigidBody.velocity = new Vector2(-m_MoveSpeed, m_RigidBody.velocity.y);
+                FlipPlayer(false);
             }
-            else if (Input.GetKeyDown(KeyCode.Space))
+            else if (Input.GetKeyDown(KeyCode.Space) && m_ActionAllowed)
             {
                 Action();
             }
         }
+    }
+
+    private void FlipPlayer(bool faceRight)
+    {
+        m_FacingRight = !faceRight;
+        m_SpriteRenderer.flipX = !faceRight;
     }
 
     private void Action()
