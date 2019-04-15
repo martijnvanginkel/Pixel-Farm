ecffb09aa6520bed0249cb4177dfb1ebcbe49b43
Martijn van Ginkel <>
2019-04-15 23:55:16 +0200

    Added a watermanager to make the water tiles flow into eachother

---
+++ Assets/Scripts/Objects/Sky & Water/WaterManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterManager : MonoBehaviour
+{
+
+    [SerializeField] private List<WaterTile> m_TileList = new List<WaterTile>();
+
+    private int m_CurrentTileInt;
+    private bool m_GoingUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddChildrenToList();
+
+
+    }
+
+    private void AddChildrenToList()
+    {
+        foreach (Transform child in this.transform)
+        {
+            m_TileList.Add(child.GetComponent<WaterTile>());
+        }
+
+        m_CurrentTileInt = m_TileList.Count - 1;
+
+        GoToNextTile();
+    }
+
+    public void GoToNextTile()
+    {
+        m_TileList[m_CurrentTileInt].StartFlow();
+
+        if(m_GoingUp == false)
+        {
+            m_CurrentTileInt--;
+
+            if(m_CurrentTileInt == 0)
+            {
+                m_GoingUp = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Sky & Water/WaterTile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTile : MonoBehaviour
+{
+    [SerializeField] private WaterManager m_WaterManager;
+    private Animator m_Animator;
+
+    private bool m_IsHigh;
+    public bool IsHigh
+    {
+        get { return m_IsHigh; }
+        set { m_IsHigh = value; }
+    }
+
+    private bool m_Flowing = false;
+    public bool Flowing
+    {
+        get { return m_Flowing; }
+        set { m_Flowing = value; }
+    }
+
+    private void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_WaterManager = FindObjectOfType<WaterManager>();
+       
+        m_Animator.SetBool("IsHigh", m_IsHigh);
+    }
+
+    public void StartFlow()
+    {
+        m_Flowing = true;
+        m_Animator.SetBool("Flowing", m_Flowing);
+        m_IsHigh = !m_IsHigh;
+    }
+
+    private void EndOfFlow()
+    {
+        m_Flowing = false;
+        m_Animator.SetBool("Flowing", m_Flowing);
+
+        m_WaterManager.GoToNextTile();
+    }
+}
